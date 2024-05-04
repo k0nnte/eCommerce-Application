37e8ab7612f143b880d4711025a33c73fc02f9a8
Bystrou Ihar <bystrov.igor25@gmail.com>
2024-05-04 03:06:22 +0300

    feat: add webpack and html plugin

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env) => {
+
+    return {
+        mode: env.mode ?? 'development',
+
+        entry: path.resolve(__dirname,'src', 'main.js'),
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: "[name].[contenthash].js",
+            clean: true,
+    
+        },
+        plugins: [
+            new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })
+        ],
+    }
+    
+   
+  };
