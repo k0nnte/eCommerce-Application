fa28b92340d25a10ba1b0ed11e47b112c6f1e027
ValeriaStav <valeriiastavriadi@gmail.com>
2024-05-15 02:43:18 +0300

    refactor: fix duplicate properties in jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,16 @@
 import type { Config } from 'jest';
 
 const jestConfig: Config = {
+  preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
-  preset: 'ts-jest',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|scss)$': 'identity-obj-proxy',
+    '\\.(png|jpg|jpeg|gif|svg)$': 'identity-obj-proxy',
+  },
   collectCoverage: true,
   coverageThreshold: {
     global: {
@@ -14,9 +19,6 @@ const jestConfig: Config = {
       functions: 25,
       lines: 25,
     },
-  },
-  moduleNameMapper: {
-    '\\.(scss|sass|css)$': 'identity-obj-proxy',
   },
 };
 
