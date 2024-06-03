488540a89863d7b16c2dd97f233da199d9733c43
Liudmila Rodzina <liudmilarodzina@gmail.com>
2024-06-03 03:10:40 +0300

    feat: (RSS-ECOMM-3_15) implement edit mode for general user information

--- src/pages/registration/registration.test.ts
+++ src/pages/registration/registration.test.ts
@@ -126,3 +126,53 @@ describe('hideError function', () => {
     expect(nonErrorElement.nextElementSibling).toEqual(originalNextSibling);
   });
 });
+
+describe('Signup Button Click Validation', () => {
+  let signUpButton: HTMLButtonElement | null;
+
+  beforeEach(() => {
+    const formContainer = document.createElement('div');
+    formContainer.classList.add('registration__form');
+    document.body.appendChild(formContainer);
+    RegistrationForm.renderRegistrationForm(formContainer);
+    signUpButton = formContainer.querySelector('.btn-submit');
+    if (!signUpButton) {
+      throw new Error('Signup button not found1');
+    }
+  });
+
+  it('should validate form fields and initiate registration process on signup button click', () => {
+    const formContainer = document.querySelector('.registration__form');
+
+    if (!formContainer) {
+      throw new Error('Form container not found');
+    }
+
+    const textInput = formContainer.querySelector(
+      'input[type="text"]',
+    ) as HTMLInputElement | null;
+    const emailInput = formContainer.querySelector(
+      'input[type="email"]',
+    ) as HTMLInputElement | null;
+    const passwordInput = formContainer.querySelector(
+      'input[type="password"]',
+    ) as HTMLInputElement | null;
+
+    if (textInput && emailInput && passwordInput) {
+      textInput.value = 'John';
+      emailInput.value = 'john@example.com';
+      passwordInput.value = 'password123';
+
+      const mockProcessCustomerRegistration = jest.fn();
+      RegistrationForm.processCustomerRegistration =
+        mockProcessCustomerRegistration;
+
+      if (signUpButton) {
+        signUpButton.click();
+        expect(mockProcessCustomerRegistration).toHaveBeenCalled();
+      } else {
+        throw new Error('Signup button not found2');
+      }
+    }
+  });
+});
