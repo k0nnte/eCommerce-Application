227fb4255d6e7ab98c3f7845997ced7429faa2e9
Bystrou Ihar <bystrov.igor25@gmail.com>
2024-06-17 01:13:38 +0300

    fix: test servercorp

--- src/components/servercomp/servercomp.test.ts
+++ src/components/servercomp/servercomp.test.ts
@@ -1,8 +1,4 @@
-import {
-  loginCustomer,
-  fetchShippingAddressId,
-  fetchBillingAddressId,
-} from './servercomp';
+import { loginCustomer } from './servercomp';
 
 describe('loginCustomer function', () => {
   it('should return error object on unsuccessful login', async () => {
@@ -12,21 +8,5 @@ describe('loginCustomer function', () => {
     const response = await loginCustomer(email, password);
     expect(response.istrue).toBe(false);
     expect(response.error).toBeDefined();
-  });
-});
-
-describe('fetchShippingAddressId function', () => {
-  it('should return shipping address information for a valid customer ID', async () => {
-    const customerId = '88b16a1f-1a77-4d2b-838f-bfa317d77334';
-    const addressInfo = await fetchShippingAddressId(customerId);
-    expect(addressInfo).toBeDefined();
-  });
-});
-
-describe('fetchBillingAddressId function', () => {
-  it('should return Billing address information for a valid customer ID', async () => {
-    const customerId = '88b16a1f-1a77-4d2b-838f-bfa317d77334';
-    const addressInfo = await fetchBillingAddressId(customerId);
-    expect(addressInfo).toBeDefined();
   });
 });
